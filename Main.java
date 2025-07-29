bd01627362f65da1b87065fae6c9b7491dbb02f3
Nicolas <nicolasrodriguesveloso@gmail.com>
2025-07-29 18:20:56 -0300

    feat: added classes Curso and Mentoria

---
+++ Main.java
@@ -0,0 +1,24 @@
+package poo.projetoabstraindobootcamp;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso = new Curso();
+
+        curso.setTitulo("Java");
+        curso.setDescricao("Java é");
+        curso.setCargaHoraria(60);
+
+        System.out.println(curso);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setNome("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+    }
+}
---
+++ Mentoria.java
@@ -0,0 +1,43 @@
+package poo.projetoabstraindobootcamp;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+    private String nome;
+    private String descricao;
+    private LocalDate data;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
