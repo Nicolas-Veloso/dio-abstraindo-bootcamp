a80aeff358a7482462095dc39a56e49efcdc10dd
Nicolas <nicolasrodriguesveloso@gmail.com>
2025-07-29 23:17:05 -0300

    feat: Implemented methods in Dev.java class

--- Dev.java
+++ Dev.java
@@ -2,6 +2,7 @@ package poo.projetoabstraindobootcamp;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,11 +11,27 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void increverBootCamp(BootCamp bootCamp) {}
+    public void increverBootCamp(BootCamp bootCamp) {
+        this.conteudosInscritos.addAll(bootCamp.getConteudos());
+        bootCamp.getDevsIncritos().add(this);
+    }
 
-    public void progredir() {}
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.out.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
 
-    public void calcularTotalXp() {}
+    public double calcularTotalXp() {
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
+    }
 
     public String getNome() {
         return nome;
