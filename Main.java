3d4356204f3237cdf86a445c33f7c82fb96ac5d7
Nicolas <nicolasrodriguesveloso@gmail.com>
2025-07-29 23:48:36 -0300

    final commit

--- Main.java
+++ Main.java
@@ -6,10 +6,15 @@ public class Main {
     public static void main(String[] args) {
 
         Curso curso = new Curso();
+        Curso curso1 = new Curso();
 
-        curso.setTitulo("Java");
-        curso.setDescricao("Java é");
-        curso.setCargaHoraria(60);
+        curso.setTitulo("Curso de Java");
+        curso.setDescricao("Descrição curso de Java");
+        curso.setCargaHoraria(10);
+
+        curso1.setTitulo("Curso de JS");
+        curso1.setDescricao("Descrição curso de JS");
+        curso1.setCargaHoraria(20);
 
         System.out.println(curso);
 
@@ -20,5 +25,37 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
         System.out.println(mentoria);
+
+        BootCamp bootCamp = new BootCamp();
+
+        bootCamp.setNome("BootCamp Java Developer");
+        bootCamp.setDescricao("Descrição bootcamp Java Developer");
+        bootCamp.getConteudos().add(curso);
+        bootCamp.getConteudos().add(curso1);
+        bootCamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Magraum");
+        dev.increverBootCamp(bootCamp);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Serjão");
+        dev1.increverBootCamp(bootCamp);
+
+        System.out.println(dev.getConteudosInscritos());
+        dev.progredir();
+        System.out.println(dev.getConteudosConcluidos());
+        System.out.println(dev.calcularTotalXp());
+
+        System.out.println("===================");
+
+        System.out.println(dev1.getConteudosInscritos());
+        System.out.println(dev1.getConteudosConcluidos());
+        dev1.progredir();
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println(dev1.getConteudosInscritos());
+        System.out.println(dev1.getConteudosConcluidos());
+        System.out.println(dev1.calcularTotalXp());
     }
 }
